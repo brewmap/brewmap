fc9d4427c2e310245b349298c778fa44def85afe
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2020-08-13 12:43:52 +0200

    #4 - Psalm added

--- config/database.php
+++ config/database.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use Illuminate\Support\Str;
 
 $prefix = Str::slug(env("APP_NAME", "laravel"), "_") . "_database_";
-$options = extension_loaded("pdo_mysql") ? array_filter([PDO::MYSQL_ATTR_SSL_CA => env("MYSQL_ATTR_SSL_CA")]) : [];
 
 return [
     "default" => env("DB_CONNECTION", "mysql"),
@@ -16,37 +15,6 @@ return [
             "database" => env("DB_DATABASE", database_path("database.sqlite")),
             "prefix" => "",
             "foreign_key_constraints" => env("DB_FOREIGN_KEYS", true),
-        ],
-        "mysql" => [
-            "driver" => "mysql",
-            "url" => env("DATABASE_URL"),
-            "host" => env("DB_HOST", "127.0.0.1"),
-            "port" => env("DB_PORT", "3306"),
-            "database" => env("DB_DATABASE", "forge"),
-            "username" => env("DB_USERNAME", "forge"),
-            "password" => env("DB_PASSWORD", ""),
-            "unix_socket" => env("DB_SOCKET", ""),
-            "charset" => "utf8mb4",
-            "collation" => "utf8mb4_unicode_ci",
-            "prefix" => "",
-            "prefix_indexes" => true,
-            "strict" => true,
-            "engine" => null,
-            "options" => $options,
-        ],
-        "pgsql" => [
-            "driver" => "pgsql",
-            "url" => env("DATABASE_URL"),
-            "host" => env("DB_HOST", "127.0.0.1"),
-            "port" => env("DB_PORT", "5432"),
-            "database" => env("DB_DATABASE", "forge"),
-            "username" => env("DB_USERNAME", "forge"),
-            "password" => env("DB_PASSWORD", ""),
-            "charset" => "utf8",
-            "prefix" => "",
-            "prefix_indexes" => true,
-            "schema" => "public",
-            "sslmode" => "prefer",
         ],
     ],
     "migrations" => "migrations",
