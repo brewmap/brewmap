0a9da5bc7ec3326dadc2f41035eef3153757b3f7
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2020-09-11 08:12:34 +0200

    #44 - adding logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+        <link rel="icon" href="{{ asset("/images/logo.png") }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/components/icon.min.css" integrity="sha512-8Tb+T7SKUFQWOPIQCaLDWWe1K/SY8hvHl7brOH8Nz5z1VT8fnf8B+9neoUzmFY3OzkWMMs3OjrwZALgB1oXFBg==" crossorigin="anonymous">
         <title>Brewmap</title>
         <style>
@@ -15,8 +16,13 @@
     <body class="h-full">
         <div class="flex flex-col min-h-screen w-full bg-cover bg-no-repeat" id="landing">
             <div class="w-full shadow-2xl bg-black bg-opacity-75 p-4">
-                <div class="flex items-end container mx-auto text-white">
-                    <a href="#" class="px-4 py-2 mr-4 leading-none font-bold text-xl">Brewmap</a>
+                <div class="flex items-center container mx-auto text-white">
+                    <a href="#" class="px-4 py-2">
+                        <img class="w-8" src="{{ asset("/images/logo.png") }}" alt="Brewmap">
+                    </a>
+                    <a href="#" class="px-4 py-2 mr-4 leading-none font-bold text-xl">
+                        Brewmap
+                    </a>
                     <a href="#" class="px-4 py-2 mr-4 leading-none">About</a>
                     <a href="#" class="px-4 py-2 mr-4 leading-none">Breweries</a>
                     <a href="#" class="px-4 py-2 mr-4 leading-none">Countries</a>
@@ -33,6 +39,7 @@
                 </div>
             </div>
         </div>
+
         <div class="bg-black shadow-2xl text-sm text-gray-500 p-4">
             <div class="container mx-auto flex">
                 <div class="flex-1">
