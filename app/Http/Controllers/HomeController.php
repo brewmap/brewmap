5f0c8a8744e95a2665a2de41c77c81ec9a38a55d
Krzysztof Rewak <krzysztof.rewak@gmail.com>
2020-09-12 11:31:40 +0200

    #44 - ECS fixups

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,10 +11,10 @@ class HomeController extends Controller
     public function __invoke(): View
     {
         $numbers = [
-            ["label" => "breweries", "value" => 1138,],
-            ["label" => "visits", "value" => 4400,],
-            ["label" => "reviews", "value" => 128,],
-            ["label" => "users", "value" => 666,],
+            ["label" => "breweries", "value" => 1138],
+            ["label" => "visits", "value" => 4400],
+            ["label" => "reviews", "value" => 128],
+            ["label" => "users", "value" => 666],
         ];
 
         return view("home")->with("numbers", $numbers);
