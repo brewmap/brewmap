801b9663918b3fb5188426d0b1a358034ff7c15a
HerringTheCoder <44547474+HerringTheCoder@users.noreply.github.com>
2020-09-27 17:02:46 +0200

    #23 - Removed sanctum config comments

--- config/sanctum.php
+++ config/sanctum.php
@@ -3,30 +3,6 @@
 declare(strict_types=1);
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Stateful Domains
-    |--------------------------------------------------------------------------
-    |
-    | Requests from the following domains / hosts will receive stateful API
-    | authentication cookies. Typically, these should include your local
-    | and production domains which access your API via a frontend SPA.
-    |
-    */
-
     "stateful" => explode(",", env("SANCTUM_STATEFUL_DOMAINS", "localhost,127.0.0.1,127.0.0.1:8000,::1")),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Expiration Minutes
-    |--------------------------------------------------------------------------
-    |
-    | This value controls the number of minutes until an issued token will be
-    | considered expired. If this value is null, personal access tokens do
-    | not expire. This won"t tweak the lifetime of first-party sessions.
-    |
-    */
-
     "expiration" => null,
 ];
