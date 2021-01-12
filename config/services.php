27a4d8e901289f615a1e845a3fd56f253b1915bf
Kamil Piech <kamil.piech.97@gmail.com>
2021-01-12 08:13:38 +0100

    Update services.php
    hide API credentials

--- config/services.php
+++ config/services.php
@@ -9,7 +9,7 @@ return [
         "redirect" => env("APP_URL") . "/facebook/callback/",
     ],
     "mailtrap" => [
-        "default_inbox" => "728700",
-        "secret" => "7f9245dea8f4621599454e42b5824ae5",
+        "default_inbox" => "",
+        "secret" => "",
     ],
 ];
